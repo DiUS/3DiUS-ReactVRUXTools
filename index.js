c685d9d33fac7549d839e0ca2ec15c2d512acd90
Matthew Butt <hellaeon@hotmail.com>
2017-06-14 17:06:51 +1000

    Set default VR view to base route

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import VRScene from './src/components/VRScene'
 
 render((
   <Router history={browserHistory}>
-    <Route path="/" component={App} />
-    <Route path="/vr-scene" component={VRScene} />
+    <Route path="/" component={VRScene} />
   </Router>
 ), document.getElementById('app'))
